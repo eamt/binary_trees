e592546744d2cdaec4ea37e58f949dda5aa6f019
eduard moreno <katananata500@gmail.com>
2021-03-12 23:54:44 +0000

    size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measuring  the size of a binary tree
+ *
+ * @tree: Root of the tree to start measuring
+ *
+ * Return: size of tree
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+}
