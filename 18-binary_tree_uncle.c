3a42d1aca0b9a7e1fe5962b49c6dec5d4d472820
eduard moreno <katananata500@gmail.com>
2021-03-13 00:41:20 +0000

    uncle and fix silbing

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - returns node's uncle
+ *
+ * @node: Node to start  find uncle
+ * Return: Uncle  node
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	if (node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+	else if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	else
+		return (NULL);
+}
